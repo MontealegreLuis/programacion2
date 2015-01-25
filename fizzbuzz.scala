d52b39e533ec621fae251e207ab5a6fe845a2cfa
Uriel Camacho <blizzcon2005@gmail.com>
2015-01-25 15:31:07 -0600

    Correcion de FizzBuzz

--- fizzbuzz.scala
+++ fizzbuzz.scala
@@ -1,13 +1,19 @@
 object FizzoBuzz{
  def main(args:Array[String]){
+    var ban=0
    for (x<-1 to 100){
-    if ((x%3) == 0)
-      println("Fizz")
-    else if ((x%5)==0)
-      println("Buzz")
-    else if (((x%3)==0)&&((x%5)==0))
+    ban=1
+    if(x%3==0&&x%5==0)
       println("FizzBuzz")
-    else
+    if ((x%3) == 0){
+      println("Fizz")
+      ban=0
+    }
+    if ((x%5)==0){
+      println("Buzz")
+      ban=0
+    }
+    if(ban==1)
       println(x)
     }
   }
