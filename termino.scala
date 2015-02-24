06b6218133ec48dc03e0463ef8bcef8c82ae0126
AlfredoCanez <alfredo_canez@hotmail.com>
2015-02-23 18:02:14 -0600

    Agrego mas codigo,espero respuesta para saber si esta bien asi

--- termino.scala
+++ termino.scala
@@ -1,26 +1,35 @@
-class termino(coeficiente: Float, exponente: Int){
+class termino(c: Float, e: Int){
+	private val coeficiente = c
+	private val exponente = e
 	
-	def agregar{
-		
-}	
-
-	var resultado: Float = 1
-	def evaluar(x:Float) {
+	def evaluar(x:Float):Float = {
+		var resultado: Float = 1	
 		for (i <- 1 to exponente){
-		      resultado = resultado*x 	
+		      resultado = resultado * x 	
 		}
 		
 		resultado = resultado * coeficiente
-}
+		resultado	
+	}
 
-	def terminosiguales:Boolean ={ 
-		if (exponente==this.exponente)
-}
+	def terminosiguales(t:termino):Boolean = { 
+		 (c != this.exponente)
+	}
 	
-	def reducir(){
+	def reducir(t:termino):termino = {		
+		if (c != this.exponente){
+		       var te = new termino(this.coeficiente+c,e)
+		       te
+		}
+		else{ 
+			t
+		}
+	}	
+	def esigual(){
 
+	}
+	def mostrar(){
 
-}	
-
+	}
 
 }
