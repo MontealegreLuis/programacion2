3dafd8915c575a0a0fc7163fe10d484a3dc862ce
aldovictoria <mb300aldo@hotmail.com>
2015-03-06 03:55:30 -0600

    polinomio en c y escala

--- tarea5/Polinomio_lectura.scala
+++ tarea5/Polinomio_lectura.scala
@@ -3,10 +3,10 @@ object Polinomio_lectura
 {
   def main(args: Array[String])
   {
-    var c = new Array[Int](2)
-    var e = new Array[Int](2)
-    var c1 = new Array[Int](2)
-    var e1 = new Array[Int](2)
+    var c = new Array[Int](3)
+    var e = new Array[Int](3)
+    var c1 = new Array[Int](3)
+    var e1 = new Array[Int](3)
 
     var suma = 0;
     var suma1 = 0;
@@ -18,10 +18,41 @@ object Polinomio_lectura
     {
       println("deme el coeficiente:"+i)
       c(i) = readInt()
+      println("deme el exponente:"+i)
+      e(i)=readInt()
     }
 
 
-  //  println(""+c(0))
+    println("deme el segundo polinomio")
+
+    for(j <- 0 to 2)
+  {
+
+    println("deme el coeficiente:"+j)
+    c1(j) = readInt()
+    println("deme el exponente:"+j)
+    e1(j)=readInt()
+
+
+  }
+
+
+  if (e(0)==e1(0))
+  {
+    suma= c1(0) + c(0)
+
+    println(suma)
+
+
+  }
+
+  if (e(1) == e1(1))
+  {
+    suma1 = c1(1) + c(1)
+    println(suma1)
+  }
+
+  println(""+suma+"x^"+e(0)+"+"+suma1)
 
   }
 }
