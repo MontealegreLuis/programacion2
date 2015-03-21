b490ef5fc0542a6aee416249883d86f437d72d12
aldovictoria <mb300aldo@hotmail.com>
2015-03-21 01:23:23 -0600

    tarea de clases abstractas

---
+++ clase_abstracto/Rectangulo_abs.scala
@@ -0,0 +1,21 @@
+class Rectangulo extends Figura {
+
+
+  /*  def dibujar(): String = {
+        "Soy un rectángulo [ ] y soy de color " + color
+    }*/
+
+    def area: Float = {
+      println("deme el tamaño de la base")
+      val b=readFloat()
+      prinln("deme el tamaño de la altura")
+      val al =readFloat()
+      val a = 0
+      a= b*a
+
+      println("el area del rectangulo es:"+a)
+
+    }
+
+
+}
