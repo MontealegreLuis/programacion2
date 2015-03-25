bf4e72ec1b399e224e14986ad5285b858e43450b
MarioIvanFrior <mario.ivan.m.p@gmail.com>
2015-03-24 21:14:31 -0600

    agrego clases de el programa universidad me produjo errores pero no supe cuales eran intente todo lo q crei y nada

---
+++ universidad/Asignatura.scala
@@ -0,0 +1,7 @@
+import com.github.nscala_time.time.Imports._
+class Asignatura(n:String,c:String,d:String,depto:String) {
+  var nombre= n
+  var codigo=c
+  var descripcion=d
+  var departamento=dpto
+}
