199241ed318c493f110aac45fe184113978c5dd8
Miguel <z_ero_mk@hotmail.com>
2015-04-08 22:55:52 -0500

    tarea polinomio en c sumar.

---
+++ suma de ecuaciones.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main()
+{
+	int a=2, b=5, c;
+	int x=3, y=-2, z;
+	printf("Primera Ecuacion: 2x + 5\n");
+	printf("Segunda Ecuacion: 3x - 2\n");
+	c=a+x;
+	z=b+y;
+	
+	printf("El resultado de la suma es %dx + %d\n",c,z);
+}
