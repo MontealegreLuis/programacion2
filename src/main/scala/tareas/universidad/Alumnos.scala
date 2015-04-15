7579521b90e6fb852f0d1718c8a7fb25fcba0f84
AbigailSanchez <abi_briza888@hotmail.com>
2015-04-15 07:25:28 -0500

    agrego programa universidad

---
+++ src/main/scala/tareas/universidad/Alumnos.scala
@@ -0,0 +1,15 @@
+package tareas.universidad
+class Alumnos{
+    val alumnos:Array[Alumnos](50)
+    var aux = 0
+        def agregar (alumnos:Alumnos){
+            alumnos(aux) = alumno
+            aux ++
+        }
+        def withmatricula(matricula: String):alumno ={
+            this.matricula = matricula
+        }
+        def total():Int {
+            aux
+        }
+}
